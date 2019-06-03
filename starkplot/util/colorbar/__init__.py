9eaa69f4faa76516b75e8b108888fe031e000257
Nathaniel Starkman <nstarkman@protonmail.com>
2019-06-03 00:35:46 -0400

    moving to colorbar folder

---
+++ starkplot/util/colorbar/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : colorbar initialization file
+# AUTHOR  : Nathaniel Starkman
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+r"""initialization file for colorbar
+"""
+
+__author__ = "Nathaniel Starkman"
+
+##############################################################################
+### Imports
+
+## General
+
+## Project-Specific
+from ._info import (
+    _colorbark,
+)
+
+
+##############################################################################
---
+++ starkplot/util/colorbar/_info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : colorbar initialization file
+# AUTHOR  : Nathaniel Starkman
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+r"""initialization file for colorbar
+"""
+
+__author__ = "Nathaniel Starkman"
+
+##############################################################################
+### Imports
+
+## General
+
+## Project-Specific
+
+
+##############################################################################
+
+_colorbark = (
+    # axes properties
+    'orientation', 'fraction', 'pad', 'shrink', 'aspect',
+    'anchor', 'panchor',
+    # colorbar properties
+    'extend', 'extendfrac', 'extendrect', 'spacing', 'ticks', 'format', 'drawedges', 'boundaries', 'values',
+)
